c65f1ced0909ecc13442637323185b6e10e9c358
Dmytro <dm.kalitventsev@gmail.com>
2023-05-30 16:26:49 +0300

    added an output of additional information about the film

--- script.js
+++ script.js
@@ -22,6 +22,18 @@ class FilmsFinder {
 		return this.dataSearchFilms[0];
 	}
 
+	get filmCards() {
+		return this.wrap.querySelector('.film-cards');
+	}
+
+	get filmCardsWrap() {
+		return this.wrap.querySelector('.film-cards__wrap');
+	}
+
+	get filmsDetailsWrap() {
+		return this.wrap.querySelector('.films-details');
+	}
+
 	getRequest(url) {
 		return new Promise((resolve, reject) => {
 			const request = (window.XMLHttpRequest) ? new XMLHttpRequest() : new ActiveXObject('Microsoft.XMLHttp');
@@ -59,27 +71,118 @@ class FilmsFinder {
 			}
 
 			formData[input.name] = value;
-
+			this.#url = `${this.#api}&s=${formData.title}&type=${formData.type}&page=1`;
 			this.firstInput.value = '';
 		}
 
 		if (!isFormValid) return;
 
-		this.#url = `${this.#api}&s=${formData.title}&type=${formData.type}`;
-
-		this.getRequest(this.#url).then(res => console.log(res));
+		this.getFilmsList(this.#url);
 	}
 
-	renderFilmsCards() {
+	getFilmsList(url) {
+		this.getRequest(url)
+			.then(res => {
+				if (res.Response === 'True') {
+					this.renderFilmsCard(res.Search);
+				};
 
+				if (res.Response === 'False') {
+					this.filmCardsWrap.innerHTML = `<div class="film-cards__not-found">${res.Error}</div>`;
+				}
+			})
+			.catch(err => console.error(err));
+	}
+
+	renderFilmsCard(dataFilms) {
+		this.filmCardsWrap.innerHTML = '';
+		
+		dataFilms.forEach(film => {
+			const cards = `<div class="film-card" data-id="${film.imdbID}">
+							<div class="film-card__poster">
+								<img src="${film.Poster}" alt="poster">
+							</div>
+							<span class="film-card__type">${film.Type}</span>
+							<h2 class="film-card__title">${film.Title}</h2>
+							<span class="film-card__year">${film.Year}</span>
+							<button class="film-card__details">Details</button>
+						</div>`
+
+			this.filmCardsWrap.insertAdjacentHTML('beforeEnd', cards);
+		});
+
+		this.filmCardsWrap.addEventListener('click', this.showDetailsFilm.bind(this));
+	}
+
+	showDetailsFilm(e) {
+		const btnDetails = e.target.closest('.film-card__details');
+
+		if (btnDetails) {
+			const id = btnDetails.parentElement.dataset.id;
+
+			this.#url = `${this.#api}&i=${id}`;
+
+			this.getRequest(this.#url)
+				.then(res => {
+					this.renderDetailsCardFilm(res);
+				})
+				.catch(err => console.error(err));
+		}
+	}
+
+	renderDetailsCardFilm(dataFilm) {
+		const str = `<h2 class="films-details__title">Film info:</h2>
+					<div class="film-details">
+						<div class="film-details__poster">
+							<img src="${dataFilm.Poster}" alt="poster">
+						</div>
+						<div class="film-details__title-wrap">
+							<span class="film-details__title">Title:</span>
+							<span class="film-details__description">${dataFilm.Title}</span>
+						</div>
+						<div class="film-details__title-wrap">
+							<span class="film-details__title">Released:</span>
+							<span class="film-details__description">${dataFilm.Released}</span>
+						</div>
+						<div class="film-details__title-wrap">
+							<span class="film-details__title">Genre:</span>
+							<span class="film-details__description">${dataFilm.Genre}</span>
+						</div>
+						<div class="film-details__title-wrap">
+							<span class="film-details__title">Country:</span>
+							<span class="film-details__description">${dataFilm.Country}</span>
+						</div>
+						<div class="film-details__title-wrap">
+							<span class="film-details__title">Director:</span>
+							<span class="film-details__description">${dataFilm.Director}</span>
+						</div>
+						<div class="film-details__title-wrap">
+							<span class="film-details__title">Writer:</span>
+							<span class="film-details__description">${dataFilm.Writer}</span>
+						</div>
+						<div class="film-details__title-wrap">
+							<span class="film-details__title">Actors:</span>
+							<span class="film-details__description">${dataFilm.Actors}</span>
+						</div>
+						<div class="film-details__title-wrap">
+							<span class="film-details__title">Awards:</span>
+							<span class="film-details__description">${dataFilm.Awards}</span>
+						</div>
+					</div>`;
+
+		this.filmsDetailsWrap.innerHTML = str;
 	}
 
 	init() {
-		this.formSearch.addEventListener('submit', this.getDataSearch.bind(this));
-		this.firstInput.addEventListener('input', () => this.firstInput.style.outline = null);
+		this.getRequest(this.#api)
+			.then(() => {
+				this.formSearch.addEventListener('submit', this.getDataSearch.bind(this));
+				this.firstInput.addEventListener('input', () => this.firstInput.style.outline = null);
+			})
+			.catch(err => {
+				document.body.innerHTML = `<div class="error">Error ${err}:(</div>`;
+			});
 	}
 }
 
 new FilmsFinder().init();
-
-// document.body.innerHTML = `i<dv class="error">Error ${request.status}:(</div>`;
